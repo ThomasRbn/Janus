d0392c2b0d09cc5332bf7afc4c4f11710ab1cf5e
Thomas ROBINEAU <thomas.robineau41@outlook.fr>
2025-07-20 16:13:15 +0200

    Add Nuxt Svgo

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,8 +4,12 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxt/eslint', '@nuxt/fonts'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', 'nuxt-svgo'],
   vite: {
     plugins: [tailwindcss()],
+  },
+  svgo: {
+    autoImportPath: '~/assets/svg/',
+    defaultImport: 'component',
   },
 })
