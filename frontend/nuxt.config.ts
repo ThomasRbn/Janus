a9c80db9f5b2d7e33b534fd81252868b5bcef5b7
Thomas ROBINEAU <thomas.robineau41@outlook.fr>
2025-07-20 16:05:56 +0200

    Add Tailwind CSS

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,11 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon']
+  css: ['~/assets/css/tailwind.css'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 })
