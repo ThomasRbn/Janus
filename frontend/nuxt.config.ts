f59b9fe5e08ef48fa9352010cb9794fac60032b1
Thomas ROBINEAU <thomas.robineau41@outlook.fr>
2025-07-20 16:11:36 +0200

    Remove Nuxt Icons

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   css: ['~/assets/css/tailwind.css'],
-  modules: ['@nuxt/eslint', '@nuxt/fonts', '@nuxt/icon'],
+  modules: ['@nuxt/eslint', '@nuxt/fonts'],
   vite: {
     plugins: [tailwindcss()],
   },
